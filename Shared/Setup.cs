fa2f7a0a69c839213765edb3b6abf30333557324
ByronKruger <byronkruger3@gmail.com>
2025-02-16 18:55:42 +0200

    Added base OTel lib

--- Shared/Setup.cs
+++ Shared/Setup.cs
@@ -1,6 +1,28 @@
-﻿namespace Shared;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using CloudinaryDotNet.Actions;
 
-public class Setup
+namespace Shared;
+
+public static class Setup
 {
+    public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration config, string appName)
+    {
+        var otel = services.AddOpenTelemetry();
 
+        otel.ConfigureResource(resource => resource
+            .AddService(
+                serviceName: appName,
+                serviceVersion: "0.0.1"
+            ))
+        .WithTracing(tracing => tracing
+            .AddSource(appName)
+            .AddAspNetCoreInstrumnetation()
+            .AddHttpClientInstrumentation()
+            .AddConsoleExporter());
+
+        return services;
+    }
 }
