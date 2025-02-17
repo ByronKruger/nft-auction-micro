b9917296257d3a89d348e208636a947cb6fad08a
ByronKruger <byronkruger3@gmail.com>
2025-02-17 15:30:05 +0200

    Changed dir-structure

---
+++ Shared/Setup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+
+namespace Shared;
+
+public static class Setup
+{
+    public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration config, string appName)
+    {
+        var otel = services.AddOpenTelemetry();
+
+        otel.ConfigureResource(resource => resource
+            .AddService(
+                serviceName: appName,
+                serviceVersion: "0.0.1"
+            ))
+        .WithTracing(tracing => tracing
+            .AddSource(appName)
+            // .AddAspNetCoreInstrumnetation()
+            // .AddHttpClientInstrumentation()
+            .AddConsoleExporter());
+
+        return services;
+    }
+}
